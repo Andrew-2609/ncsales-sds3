490e7b3b8e94504b321f0497fa41f8496e161155
Andrew Monteiro <andrew.agoravai@gmail.com>
2021-05-05 22:34:35 -0300

    Navbar updated

--- frontend/src/components/NavBar/index.tsx
+++ frontend/src/components/NavBar/index.tsx
@@ -1,11 +1,14 @@
-import ImgDsDark from 'assets/img/ds_dark.svg';
+import ImgLogo from 'assets/img/my_logo.png';
 
 const NavBar = () => {
     return (
         <div className="d-flex flex-column flex-md-row align-items-center p-3 px-md-4 mb-3 bg-light border-bottom shadow-sm">
             <div className="container">
                 <nav className="my-2 my-md-0 mr-md-3">
-                    <img src={ImgDsDark} alt="DevSuperior" width="120" />
+                    <p className="h6 text-secondary">
+                    <img src={ImgLogo} alt="DevSuperior" width="50" />
+                    NdrewCoding
+                    </p>
                 </nav>
             </div>
         </div>
