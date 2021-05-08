5ee0ff01231984657218378e9c9b7c6c96b18a55
Andrew Monteiro <andrew.agoravai@gmail.com>
2021-05-08 14:40:30 -0300

    BarChart integration

--- frontend/src/types/sale.ts
+++ frontend/src/types/sale.ts
@@ -1,4 +1,10 @@
 export type SaleSum = {
-    sellerName: string,
-    salesSum: number
+    sellerName: string;
+    salesSum: number;
+}
+
+export type SaleSuccess = {
+    sellerName: string;
+    visited: number;
+    deals: number;
 }
